1d24f207edfbd7e5d8d1330dc61bd665ae59b28b
tiagorg <tiagorg@rendlaptop>
2022-10-15 23:17:04 +0100

    Some comments

--- 1ano/fp/aula04/divs-cate.py
+++ 1ano/fp/aula04/divs-cate.py
@@ -1,26 +1,15 @@
-def divList(n):
-    divs = []
-    for x in range(1, n):
-        if n % x == 0:
-            divs.append(x)
-    return divs
-
-def category(n):
-    total = 0
-    for div in divList(n):
-        total += div
-    if total < n: return 'deficiente'
-    if total == n: return 'perfeito'
-    if total > n: return 'abundante'
-
+# Função principal
 def main():
     n = int(input('Introduza um número: '))
-    div_list = ""
     div_list_array = divList(n)
+
+    # Transforma a lista obtida numa string para imprimir
+    div_list = ""
     for div in div_list_array:
-        div_list += str(div)
-        if div != div_list_array[len(div_list_array)-1]:
-            div_list += ', '
+        div_list += (str(div) + ', ')
+    div_list = div_list[:len(div_list)-2]
+    # ---
+    
     print("""
 
 --------------------
@@ -34,6 +23,23 @@ Este é um número {}.
 
 --------------------
 
-""".format(n, div_list, category(n)))
+""".format(n, div_list, category(n, div_list_array)))
+
+# Obtém uma lista com todos os dividores de um número
+def divList(n):
+    divs = []
+    for x in range(1, n):
+        if n % x == 0:
+            divs.append(x)
+    return divs
+
+# Obtém a categoria de um número
+def category(n, divs):
+    total = 0
+    for div in divs:
+        total += div
+    if total < n: return 'deficiente'
+    if total == n: return 'perfeito'
+    if total > n: return 'abundante'
 
 main()
