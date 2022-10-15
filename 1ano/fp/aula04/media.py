37104c53e39733e316686e6e6e3ed9d5fc6a160a
tiagorg <tiagorg@rendlaptop>
2022-10-15 23:17:04 +0100

    Some comments

--- 1ano/fp/aula04/media.py
+++ 1ano/fp/aula04/media.py
@@ -1,5 +1,7 @@
+# Cria uma lista acessivel por todas as funções
 values = []
 
+# Pede ao utilizadores todas as parcelas e adiciona-as à lista 'values'
 def GetValues():
     c = 1
     while True:
@@ -8,12 +10,14 @@ def GetValues():
         values.append(float(n))
         c += 1
 
+# Calcula a média dos valores da lista 'values'
 def GetMedia(val):
     total = 0
     for v in val:
         total += v
     return total / len(val)
 
+# Função principal
 def main():
     GetValues()
     print('Média dos valores introduzidos: ', GetMedia(values))
