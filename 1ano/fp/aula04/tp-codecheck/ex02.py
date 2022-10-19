30c5d3b99efe306935a4ac05194427b70ba422fd
tiagorg <tiagorg@rendlaptop>
2022-10-19 09:47:23 +0100

    1ano/fp/aula04: tp04 codecheck exercises added 1ano/fp/aula03/ex12.py: comment added

---
+++ 1ano/fp/aula04/tp-codecheck/ex02.py
@@ -0,0 +1,19 @@
+# Read numbers until a sentinel (an empty string), and return their sum.
+# JMR 2018
+
+
+def inputTotal():
+   """Read numbers until empty string is entered and return the sum."""
+   tot = 0.0
+   while True:
+      n = input("valor? ") # input("valor? ")
+      if n == '': return tot
+      tot += float(n)
+
+# MAIN PROGRAM
+def main():
+   tot = inputTotal()
+   print(tot)
+
+if __name__ == "__main__":
+   main()
---
+++ 1ano/fp/aula04/tp-codecheck/ex03.py
@@ -0,0 +1,23 @@
+# Example: Print a multiplication table
+# Like:
+#   1 x 1  =   1
+#   1 x 2  =   2
+#   ...
+#  10 x 10 = 100
+#
+# Make the program print an empty line after each group of ten.
+# Also, use format specifiers to align the numbers.
+#
+# JMR 2019   
+
+def main():
+   for i in range (1, 11):
+      table(i)
+      print()
+
+def table(n):
+   for i in range(1, 11):
+      print(f'{n} x {i} = {n*i}')
+
+if __name__ == "__main__":
+   main()
