5403e6059649b179fa4c3c904ae9cf303b5177dd
tiagorg <tiagorg@rendlaptop>
2022-10-21 16:59:09 +0100

    Added Tp05 CodeCheck exercises

---
+++ 1ano/fp/aula05/tp05-codecheck/exercises02/ex01.py
@@ -0,0 +1,32 @@
+# Substitua as reticências pelo valor adequado para obter as respostas pedidas.
+
+# Considere que num comboio de passageiros
+# as primeiras N carruagens têm lugares de primeira classe
+# enquanto as restantes têm lugares de segunda classe.
+
+# Complete a definição para que passengers(train, n)
+# devolva uma lista com dois elementos, [num1, num2], onde:
+# num1 = número de passageiros em primeira classe (nas n primeiras carruagens)
+# num2 = número de passageiros em segunda classe.
+def passengers(train, n):
+    class1 = train[:n]
+    class2 = train[n:]
+    num1 = sum(class1)
+    num2 = sum(class2)
+    return [num1, num2]
+
+def test():
+    train1 = [12, 32, 10, 21]
+    train2 = [9, 29, 19]
+    train3 = [14, 34, 24]
+    print(train1, train2, train3)
+    
+    print('\nTesting passengers')
+    print(passengers(train1, 2))
+    print(passengers(train2, 1))
+    print(passengers(train3, 0))
+    
+# Run tests:
+test()
+
+# JMR@ua.pt 2021
---
+++ 1ano/fp/aula05/tp05-codecheck/exercises03/ex02.py
@@ -0,0 +1,26 @@
+# Substitua as reticências pelo valor adequado para obter as respostas pedidas.
+
+# Este tuplo define os nomes dos meses em Português:
+MESESPT = ("janeiro", "fevereiro", "março", "abril", "maio", "junho",
+           "julho", "agosto", "setembro", "outubro", "novembro", "dezembro")
+
+# Complete a função para que, dado um tuplo (ano, mes, dia)
+# devolva um data por extenso.
+# Por exemplo, datePT((1938, 1, 22)) deve devolver "22 de janeiro de 1938".
+def datePT(date):
+    ano, mes, dia = date
+    s = f"{str(dia)} de {MESESPT[mes-1]} de {ano}"
+    return s
+
+# Complete a definição para converter uma data no formato "DD/MM/AAAA"
+# num tuplo de inteiros com (ano, mês, dia).
+# Por exemplo: parseDMY("25/12/2020") deve devolver (2020, 12, 25).
+def parseDMY(s):
+    # Sugestão: use o método split.
+    parts = s.split('/')
+    return (int(parts[2]), int(parts[1]), int(parts[0]))
+
+# No CodeCheck estas funções são testadas com argumentos pré-programados.
+# Se quiser testar offline, tem de acrescentar as suas intruções de chamada.
+
+# JMR@ua.pt 2021
