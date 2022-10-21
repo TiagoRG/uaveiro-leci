1694ced5f3ac4a736976fb5106936826d3d7ebea
tiagorg <tiagorg@rendlaptop>
2022-10-21 16:58:49 +0100

    Removed darts.cs

--- 1ano/fp/aula02/darts.py
+++ 1ano/fp/aula02/darts.py
@@ -13,80 +13,31 @@ Ambas em milimetros.
 
     if mod > 170:
         print('Fora do alvo.')
-        exit(1)
+        return
 
     if mod < 12.7:
         print('Pontuacao: 50 pontos.')
-        exit(1)
+        return
     elif mod < 32:
         print('Pontuacao: 25 pontos.')
-        exit(1)
+        return
 
-    base_point = BasePoint(x, y)
-
+    score = BasePoint(x, y)
     if mod > 99 and mod < 107:
-        print('Pontuacao: {}pontos.'.format(base_point * 3))
-        exit(1)
+        score *= 3
     if mod > 162:
-        print('Pontuacao: {}pontos.'.format(base_point * 2))
-        exit(1)
-
-    print('Pontuacao: {}pontos.'.format(base_point))
+        score *= 2
+        
+    print(f'Pontuacao: {score} pontos.')
     exit(1)
 
 def BasePoint(x, y):
-    if x > 0:
-        if abs(y) < x * math.tan(math.pi / 20):
-            return 6
-        else:
-            if y > 0:
-                if y < x * math.tan(3 * math.pi / 20):
-                    return 13
-                if y < x * math.tan(5 * math.pi / 20):
-                    return 4
-                if y < x * math.tan(7 * math.pi / 20):
-                    return 18
-                if y < x * math.tan(9 * math.pi / 20):
-                    return 1
-                else:
-                    return 20
-            else:
-                if y > x * math.tan(-3 * math.pi / 20):
-                    return 10
-                if y > x * math.tan(-5 * math.pi / 20):
-                    return 15
-                if y > x * math.tan(-7 * math.pi / 20):
-                    return 2
-                if y > x * math.tan(-9 * math.pi / 20):
-                    return 17
-                else:
-                    return 3
-    else:
-        if abs(y) < x * math.tan(math.pi + math.pi / 20):
-            return 6
-        else:
-            if y > 0:
-                if y < x * math.tan(math.pi + 3 * math.pi / 20):
-                    return 14
-                if y < x * math.tan(math.pi + 5 * math.pi / 20):
-                    return 9
-                if y < x * math.tan(math.pi + 7 * math.pi / 20):
-                    return 12
-                if y < x * math.tan(math.pi + 9 * math.pi / 20):
-                    return 5
-                else:
-                    return 20
-            else:
-                if y > x * math.tan(math.pi + -3 * math.pi / 20):
-                    return 8
-                if y > x * math.tan(math.pi + -5 * math.pi / 20):
-                    return 16
-                if y > x * math.tan(math.pi + -7 * math.pi / 20):
-                    return 7
-                if y > x * math.tan(math.pi + -9 * math.pi / 20):
-                    return 19
-                else:
-                    return 3
+    angleRad = math.atan2(y, x)
+    angleDeg = math.degrees(angleRad) - 9
+    
+    POINTS = (6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10)
 
+    return POINTS[int(angleDeg / 20)]
+    
 if __name__ == '__main__':
     main()
