bcb84f0b465847e5516eab63db231d6abe3364be
tiagorg <tiagorg@rendlaptop>
2022-11-09 21:56:58 +0000

    allMatches update

--- 1ano/fp/aula05/allMatches.py
+++ 1ano/fp/aula05/allMatches.py
@@ -3,13 +3,15 @@ def main():
     print(matches)
     print(len(matches))
 
+
 def allMatches(teamList):
     matchList = []
+
     for team1 in teamList:
         for team2 in teamList:
-            if team1 == team2:
-                continue
-            matchList.append((team1, team2))
+            if team1 != team2:
+                matchList.append((team1, team2))
+
     return matchList
 
 if __name__ == "__main__":
