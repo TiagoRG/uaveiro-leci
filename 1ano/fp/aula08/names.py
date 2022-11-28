36c0cffa3ad4d3aa90fee4707b3fece68802dd22
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2022-11-28 18:01:03 +0000

    Converted names.py to a list comprehension

--- 1ano/fp/aula08/names.py
+++ 1ano/fp/aula08/names.py
@@ -1,12 +1,9 @@
 def main():
     with open('names.txt', 'r') as f:
         names = f.read().split('\n')
-        surnames = set([name.split(' ')[-1] for name in names[1:]])
-        namesDict = {}
-        for surname in surnames:
-            namesDict[surname] = set([name.split(' ')[0] for name in names[1:] if surname in name and surname != ''])
+        namesDict = {surname: set([name.split(' ')[0] for name in names[1:] if surname in name])
+                     for surname in set([name.split(' ')[-1] for name in names[1:] if name != ''])}
 
-        del namesDict['']
         printNames(namesDict)
 
 
