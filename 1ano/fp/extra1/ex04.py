6ef623891580f7e547d3a219dce90e57a84fd3a2
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2022-12-01 22:45:37 +0000

    extras/ex04 added

---
+++ 1ano/fp/extra1/ex04.py
@@ -0,0 +1,17 @@
+# Not passing codecheck tests
+
+def hondt(votes, numseats):
+    v = [vote for vote in votes]
+    seats = [0] * len(votes)
+    for i in range(numseats):
+        maxvotes = max(votes)
+        maxindex = votes.index(maxvotes)
+        maxvotes2 = max(votes[maxindex:])
+        maxindex2 = votes.index(maxvotes2)
+        if (maxvotes == maxvotes2) and (v[maxindex] < v[maxindex2]):
+            seats[maxindex2] += 1
+            votes[maxindex2] = maxvotes2 / (seats[maxindex2] + 1)
+        else:
+            seats[maxindex] += 1
+            votes[maxindex] = maxvotes / (seats[maxindex] + 1)
+    return seats
