c24adbf4356d17e479a809567ee92be203edf35d
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-01-18 19:02:26 +0000

    FP: Extra3 - Ex11 simplification ISD: Quick fix

--- 1ano/fp/extra3/ex11.py
+++ 1ano/fp/extra3/ex11.py
@@ -1,11 +1,4 @@
 def onlyCaps(s):
     # NOTE: ch.isupper() -> True if ch is uppercase.
-    if len(s) == 0:
-        return ""
-    if len(s) == 1:
-        return s if s.isupper() else ""
-    return s[0] + onlyCaps(s[1:]) if s[0].isupper() else onlyCaps(s[1:])
+    return (s if len(s) == 0 or (len(s) == 1 and s.isupper()) else "") if len(s) <= 1 else (s[0] + onlyCaps(s[1:]) if s[0].isupper() else onlyCaps(s[1:]))
 
-
-print(onlyCaps("John Fitzgerald Kennedy"))
-print(onlyCaps(""))
