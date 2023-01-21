0fe480b7d4948cac937772bc223a9a9d8aef403d
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-01-21 17:56:24 +0000

    Quick update

--- 1ano/fp/avaliacao/ap2/ex9.py
+++ 1ano/fp/avaliacao/ap2/ex9.py
@@ -1,3 +1,10 @@
+"""
+No jogo Bulls-and-Cows, o jogador tenta descobir um código secreto composto por N símbolos de um alfabeto pequeno, por exemplo algarismos ou letras. Em cada tentativa, o jogador introduz N símbolos e o computador dá uma pontuação que indica quantos símbolos estão certos na posição certa (os Bulls) e quantos estão certos, mas na posição errada (as Cows). Por exemplo, se o segredo for '4271', a tentativa '1234' tem uma pontuação de 1 Bull (o ‘2’) e 2 Cows (‘1’ e ‘4’).
+
+Complete a função score(guess, secret) para devolver a pontuação de uma tentativa. Por exemplo, score('1234', '4271') deve devolver (1, 2).
+"""
+
+
 def score(guess, secret):
     assert len(guess) == len(secret)
 
@@ -17,3 +24,4 @@ def score(guess, secret):
                 cows_index.append(i)
 
     return len(bulls_index), len(cows_index)
+
