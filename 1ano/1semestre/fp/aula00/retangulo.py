0e31db9b1a30a9bdf74338ca8bf970d60f05a55c
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-01-30 22:54:24 +0000

    aula00 added

---
+++ 1ano/1semestre/fp/aula00/retangulo.py
@@ -0,0 +1,8 @@
+largura = float(input("Largura? "))
+altura = float(input("Altura? "))
+
+area = largura * altura
+perimetro = largura*2 + altura*2
+
+print("Area:", area)
+print("Perimetro:", perimetro)
