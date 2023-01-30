52137abfe76f201cd0527d20a46d2617d05c795c
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-01-30 17:50:10 +0000

    Divided 1ano folders into 1semestre and 2semestre (hidden for now)

---
+++ 1ano/1semestre/fp/aula01/ex03.py
@@ -0,0 +1,6 @@
+v1 = float(input('Velocidade de ida (v1): '))
+v2 = float(input('Velocidade de regresso (v2): '))
+
+vm = (2 * v1 * v2) / (v1 + v2)
+
+print('\nA velocidade média da viagem completa foi: ', vm)
