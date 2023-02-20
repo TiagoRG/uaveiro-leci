12583ba102468bdd1f6ce98132a39b0195968e4e
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-02-20 22:33:02 +0000

    POO dates format

--- 1ano/2semestre/poo/src/aula02/SecsToHMS.java
+++ 1ano/2semestre/poo/src/aula02/SecsToHMS.java
@@ -15,10 +15,10 @@ public class SecsToHMS {
         System.out.println("Introduza os segundos totais: ");
         int totalSecs = (int) UserInput.getPositiveNumber(sin);
         int secs = totalSecs % 60;
-        int mins = Math.round((float) (totalSecs / 60));
-        int hours = Math.round((float) (mins / 60));
+        int mins = totalSecs / 60;
+        int hours = mins / 60;
         mins = mins % 60;
-        System.out.printf("%d segundos no formato hh:mm:ss : %d:%d:%d\n", totalSecs, hours, mins, secs);
+        System.out.printf("%d segundos no formato hh:mm:ss : %02d:%02d:%02d\n", totalSecs, hours, mins, secs);
 
         sin.close();
     }
