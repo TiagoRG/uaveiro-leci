5297ca6829a7a1b0d198a4bfb04723e0e0aa8147
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-03-06 15:20:46 +0000

    [POO] aula04 ex3 added

---
+++ 1ano/2semestre/poo/src/aula04/Car.java
@@ -0,0 +1,21 @@
+package aula04;
+
+class Car {
+    public String make;
+    public String model;
+    public int year;
+    public int kms;
+
+    public Car(String make, String model, int year, int kms) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.kms = kms;
+    }
+
+    public void drive(int distance) {
+        //TODO: acumular distância percorrida
+        this.kms += distance;
+    }
+
+}
