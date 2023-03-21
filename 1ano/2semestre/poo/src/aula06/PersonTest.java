469cc51a11f508f4b1b85c48ac09a885fa9056e4
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-03-21 19:00:48 +0000

    [POO] removed regex for names

--- 1ano/2semestre/poo/src/aula06/PersonTest.java
+++ 1ano/2semestre/poo/src/aula06/PersonTest.java
@@ -7,9 +7,9 @@ public class PersonTest {
     public static void main(String[] args) {
         Scanner sin = new Scanner(System.in);
 
-        Student al = new Student ("Andreia Melo", 9855678,new DateYMD(18, 7, 1990), new DateYMD(1, 9, 2018));
-        Professor p1 = new Professor("Jorge Almeida", 3467225, new DateYMD(13, 3, 1967), "Associado", "Informática");
-        Bolser bls = new Bolser ("Igor Santos", 8976543, new DateYMD(11, 5, 1985), p1, 900);
+        Student al = new Student ("Andreia Melo", 98556781,new DateYMD(18, 7, 1990), new DateYMD(1, 9, 2018));
+        Professor p1 = new Professor("Jorge Almeida", 34672215, new DateYMD(13, 3, 1967), "Associado", "Informática");
+        Bolser bls = new Bolser ("Igor Santos", 89765431, new DateYMD(11, 5, 1985), p1, 900);
         bls.setMonthlyAmount(1050);
         System.out.println("Student:"+ al.getName());
         System.out.println(al);
