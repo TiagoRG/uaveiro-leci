60944a922806272205a0d6e949b31c1da21147f5
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-03-28 23:34:18 +0100

    [POO] aula07 shapes update

--- 1ano/2semestre/poo/src/aula07/ex1/Rectangle.java
+++ 1ano/2semestre/poo/src/aula07/ex1/Rectangle.java
@@ -1,14 +1,14 @@
 package aula07.ex1;
 
+import java.util.Objects;
+
 public class Rectangle extends Shape {
     public double side1;
     public double side2;
 
-    public Rectangle(double side1, double side2) {
-        if (!(side1 > 0 && side2 > 0))
-            throw new IllegalArgumentException("Sizes must be positive.");
-        this.side1 = side1;
-        this.side2 = side2;
+    public Rectangle(String color, double side1, double side2) {
+        this.setColor(color);
+        this.setSides(side1, side2);
     }
 
     public double[] getSides() {
@@ -22,10 +22,18 @@ public class Rectangle extends Shape {
         this.side2 = side2;
     }
 
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     @Override
     public boolean equals(Shape s2) {
         if (s2 instanceof Rectangle s2Rect)
-            return this.side1 == s2Rect.side1 && this.side2 == s2Rect.side2;
+            return Objects.equals(this.color, s2Rect.color) &&  this.side1 == s2Rect.side1 && this.side2 == s2Rect.side2;
         return false;
     }
 
--- 1ano/2semestre/poo/src/aula07/ex1/Triangle.java
+++ 1ano/2semestre/poo/src/aula07/ex1/Triangle.java
@@ -1,16 +1,15 @@
 package aula07.ex1;
 
+import java.util.Objects;
+
 public class Triangle extends Shape {
     private double side1;
     private double side2;
     private double side3;
 
-    public Triangle(double side1, double side2, double side3) {
-        if (!(side1 > 0 && side2 > 0 && side3 > 0))
-            throw new IllegalArgumentException("Sizes must be positive.");
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+    public Triangle(String color, double side1, double side2, double side3) {
+        this.setColor(color);
+        this.setSides(side1, side2, side3);
     }
 
     public double[] getSides() {
@@ -25,10 +24,18 @@ public class Triangle extends Shape {
         this.side3 = side3;
     }
 
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     @Override
     public boolean equals(Shape s2) {
         if (s2 instanceof Triangle s2Triang)
-            return this.side1 == s2Triang.side1 && this.side2 == s2Triang.side2 && this.side3 == s2Triang.side3;
+            return Objects.equals(this.color, s2Triang.color) && this.side1 == s2Triang.side1 && this.side2 == s2Triang.side2 && this.side3 == s2Triang.side3;
         return false;
     }
 
