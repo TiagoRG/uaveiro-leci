2630762c1f97350516eb01eafb58057b248d8b59
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-04-04 22:05:09 +0100

    [LSD] pratica04 CounterDemo enable/disable added and added FreqDevider by itself

--- 1ano/2semestre/lsd/pratica04/CounterDemo/Bin7SegDecoder.vhd
+++ 1ano/2semestre/lsd/pratica04/CounterDemo/Bin7SegDecoder.vhd
@@ -4,14 +4,16 @@ use IEEE.STD_LOGIC_1164.all;
 entity Bin7SegDecoder is
 	port
 	(
-		binInput : in std_logic_vector(3 downto 0);
+		binInput : in  std_logic_vector(3 downto 0);
+		enable   : in  std_logic;
 		decOut_n : out std_logic_vector(6 downto 0)
 	);
 end Bin7SegDecoder;
 
 architecture Behavioral of Bin7SegDecoder is
 begin
-	decOut_n <= "1111001" when (binInput = "0001") else --1
+	decOut_n <= "0111111" when (enable   = '0'   ) else -- disabled
+	            "1111001" when (binInput = "0001") else --1
 					"0100100" when (binInput = "0010") else --2
 					"0110000" when (binInput = "0011") else --3
 					"0011001" when (binInput = "0100") else --4
