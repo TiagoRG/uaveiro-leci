29e977c1099a02486a39ac120d3fd9edafbc626c
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-04-08 13:07:08 +0100

    [POO] aula08 ex3 added

---
+++ 1ano/2semestre/poo/src/aula08/ex3/Interfaces/IPurchase.java
@@ -0,0 +1,9 @@
+package aula08.ex3.Interfaces;
+
+import aula08.ex3.Product;
+
+public interface IPurchase {
+    void addProduct(Product product, int amount);
+    void listProducts();
+    double getTotal();
+}
