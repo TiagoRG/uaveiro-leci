920b85eafb2a4176bbc123ea67f71a707b85ba9d
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-05-02 11:20:41 +0100

    [POO] aula09 added

---
+++ 1ano/2semestre/poo/src/aula09/ex1/ALDemo.java
@@ -0,0 +1,42 @@
+package aula09.ex1;
+
+import aula06.ex1.Person;
+import utils.DateYMD;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ALDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> c1= new ArrayList<>();
+        for(int i= 10; i<= 100; i+=10) c1.add(i);System.out.println("Size: "+ c1.size());
+
+        for(int i= 0; i< c1.size(); i++)
+            System.out.println("Elemento: "+ c1.get(i));
+
+        ArrayList<String> c2= new ArrayList<>();
+        c2.add("Vento");
+        c2.add("Calor");
+        c2.add("Frio");
+        c2.add("Chuva");
+        System.out.println(c2);
+        Collections.sort(c2);
+        System.out.println(c2);
+        c2.remove("Frio");
+        c2.remove(0);
+        System.out.println(c2);
+
+        Set<Person> c3 = new HashSet<>();
+        c3.add(new Person("Ana", 12345678, new DateYMD(1, 1, 2000)));
+        c3.add(new Person("Joao", 42342289, new DateYMD(1, 1, 2000)));
+        c3.add(new Person("Maria", 12346789, new DateYMD(1, 1, 2000)));
+        c3.add(new Person("Marco", 12356789, new DateYMD(1, 1, 2000)));
+        c3.add(new Person("Ana", 12346789, new DateYMD(1, 1, 2000)));
+
+        for (Person p : c3) {
+            System.out.println(p);
+        }
+    }
+}
---
+++ 1ano/2semestre/poo/src/aula09/ex2/CollectionTester.java
@@ -0,0 +1,46 @@
+package aula09.ex2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionTester {
+    public static void main(String[] args) {
+        int DIM= 5000;
+        Collection<Integer> col= new ArrayList<>();
+        checkPerformance(col, DIM);
+    }
+
+    private static void checkPerformance(Collection<Integer> col, int DIM) {
+        double start, stop, delta;
+        // Add
+        start = System.nanoTime(); // clock snapshot before
+        for(int i=0; i<DIM; i++)
+            col.add(i);
+        stop = System.nanoTime();  // clock snapshot after
+        delta = (stop-start)/1e6;  // convert to milliseconds
+        System.out.println(col.size()+ ": Add to "+col.getClass().getSimpleName() +" took "+ delta+ "ms");
+
+        // Search
+        start = System.nanoTime(); // clock snapshot before
+        for(int i=0; i<DIM; i++) {
+            int n = (int) (Math.random()*DIM);
+            if(!col.contains(n))
+                System.out.println("Not found???"+n);
+        }
+        stop = System.nanoTime();  // clock snapshot after
+        delta = (stop-start)/1e6;  // convert nanoseconds to milliseconds
+        System.out.println(col.size()+ ": Search to "+ col.getClass().getSimpleName() +" took "+ delta+ "ms");
+
+        // Remove
+        start = System.nanoTime(); // clock snapshot before
+        Iterator<Integer> iterator = col.iterator();
+        while(iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+        }
+        stop = System.nanoTime();  // clock snapshot after
+        delta = (stop-start)/1e6;  // convert nanoseconds to milliseconds
+        System.out.println(col.size() + ": Remove from "+ col.getClass().getSimpleName() +" took "+ delta+ "ms");
+    }
+}
