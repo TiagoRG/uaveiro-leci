4f3c1a9f6d3d88c4db4dbaa5e5d9ec584d9582ed
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-05-12 21:06:42 +0100

    [POO] aula11: implemented SimpleGradeCalculator

--- 1ano/2semestre/poo/src/aula11/ex2/Gradebook.java
+++ 1ano/2semestre/poo/src/aula11/ex2/Gradebook.java
@@ -1,15 +1,14 @@
 package aula11.ex2;
 
-import utils.MathTools;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class Gradebook implements IGradeCalculator {
+public class Gradebook {
     private final LinkedList<Student> students = new LinkedList<>();
+    private final IGradeCalculator gradeCalculator = new SimpleGradeCalculator();
 
     public void load(String filename) {
         LinkedList<String> lines = new LinkedList<>();
@@ -42,16 +41,11 @@ public class Gradebook implements IGradeCalculator {
     }
 
     public double calculateAverageGrade(String name) {
-        return calculate(getStudent(name).getGrades());
-    }
-
-    @Override
-    public double calculate(LinkedList<Double> grades) {
-        return MathTools.media(grades);
+        return gradeCalculator.calculate(getStudent(name).getGrades());
     }
 
     public void printAllStudents() {
         for (Student student : students)
-            System.out.printf("Nome: %s%nNota Final: %.2f%n%n", student.getName(), calculate(student.getGrades()));
+            System.out.printf("Nome: %s%nNota Final: %.2f%n%n", student.getName(), gradeCalculator.calculate(student.getGrades()));
     }
 }
