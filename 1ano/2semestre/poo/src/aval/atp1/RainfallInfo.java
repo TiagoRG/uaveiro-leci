dd843435b047559d9f045a66c9ff860b2b570c0a
TiagoRG <tiago.rgarcia@ua.pt>
2023-05-30 14:38:01 +0100

    [POO] ATP1 added
    --> Exercise made in Codecheck

---
+++ 1ano/2semestre/poo/src/aval/atp1/RainfallInfo.java
@@ -0,0 +1,38 @@
+package aval.atp1;
+
+import java.time.LocalDate;
+// YOU MAY ADD IMPORTS HERE
+
+public class RainfallInfo {
+    private LocalDate date;
+    private String location;
+    private double rainfall;
+    public RainfallInfo(LocalDate date, String location, double rainfall) {
+        this.date = date;
+        this.location = location;
+        this.rainfall=rainfall;
+    }
+    public LocalDate date() {
+        return date;
+    }
+
+    public String location() {
+        return location;
+    }
+
+    public double rainfall() {
+        return rainfall;
+    }
+
+    // YOU MAY ADD METHODS HERE
+    @Override
+    public String toString() {
+        return String.format("(%s, %s, %.1f)", date(), location(), rainfall());
+    }
+
+
+    @Override
+    public int hashCode() {
+        return date.hashCode() | location.hashCode() | Double.hashCode(rainfall);
+    };
+}
