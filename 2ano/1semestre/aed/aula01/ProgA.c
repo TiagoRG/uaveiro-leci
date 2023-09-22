0010907e6a831f50f00bf27ee0d96a5ee2429ce5
TiagoRG <tiago.rgarcia@ua.pt>
2023-09-22 12:10:38 +0100

    [AED] aula01 ProgA fix

--- 2ano/1semestre/aed/aula01/ProgA.c
+++ 2ano/1semestre/aed/aula01/ProgA.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-int arr_length(int arr[])
-{
-    int count = 0;
-    for(int i=0; arr[i]!='\0'; i++) {
-        count++;
-    }
-    return count;
-}
-
-void print_array(char s[], int arr[]) {
+void print_array(char s[], int arr[], int arr_size) {
     printf("%s:\n", s);
-    for (int i=0; i<arr_length(arr); i++) {
+    for (int i=0; i<arr_size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-void cumSum(int a[], int b[]) {
+void cumSum(int a[], int b[], int arr_size) {
     int c = 0;
-    for (int i=0; i<arr_length(a); i++) {
+    for (int i=0; i<arr_size; i++) {
         c += a[i];
         b[i] = c;
     }
@@ -27,10 +18,8 @@ void cumSum(int a[], int b[]) {
 
 int main() {
     int a[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    print_array("a", a);
-
-    int b[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    //print_array("b", b);
-    cumSum(a, b);
-    print_array("b", b);
+    print_array("a", a, sizeof(a)/sizeof(*a));
+    int b[12] = {0};
+    cumSum(a, b, sizeof(a)/sizeof(*a));
+    print_array("b", b, sizeof(b)/sizeof(*b));
 }
